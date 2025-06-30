4f09718ff0f0c8da4d7d8d2e2bded0475f271620
Moa Karlsson <moa1989@gmail.com>
2025-06-30 21:41:22 +0100

    removed brackets in html file. Made parameters to bytes to match binary read of file in do_GET().

--- server.py
+++ server.py
@@ -5,19 +5,23 @@ import mimetypes
 class handler(BaseHTTPRequestHandler):
         
     def do_GET(self):
+        db = Db()
         file_path = "." + self.path
+        english_sentence, spanish_sentence = db.find_by_id(1)
 
         try:
             with open(file_path, "rb") as file:  # rb means read binary
                 content = file.read()
+                content2 = content.replace(b"english_sentence", english_sentence.encode("utf-8"))  # Both parameters need to be in bytes since content is binary
+
                 self.send_response(200)
 
                 # Guesses MIME-typ: the html file in general will be html, and the jpg-img will be jpg.
                 mime_type, _ = mimetypes.guess_type(file_path)
                 self.send_header("Content-type", mime_type or "application/octet-stream")  # send_header sends meta info to client
                 self.end_headers()  # appl... is default for binary files if guess can't be made
+                self.wfile.write(content2)
 
-                self.wfile.write(content)
         except FileNotFoundError:
             self.send_response(404)
             self.end_headers()
@@ -55,6 +59,13 @@ class handler(BaseHTTPRequestHandler):
 
         self.wfile.write(bytes(message, "utf8"))
 
+class Db:
+    spanish_answers = ["Hola", "Buenos dias"]
+    english_answers = ["Hi", "Good morning"]
+
+    def find_by_id(self, id):
+        return (self.spanish_answers[id - 1], self.english_answers[id - 1])
+        
 
 with HTTPServer(("", 8000), handler) as server:
     server.serve_forever()
